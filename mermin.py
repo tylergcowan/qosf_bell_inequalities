ef82eb0c8f52ae4730538743089603f5e0224805
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-12-12 20:54:23 -0500

    update mermin measurements
    measurements finalized for iGHZ state

--- mermin.py
+++ mermin.py
@@ -1,7 +1,7 @@
 from qiskit import QuantumCircuit
 from pytket.extensions.qiskit import IBMQBackend
 from pytket import Circuit
-from pytket.extensions.qiskit import tk_to_qiskit, qiskit_to_tk
+from pytket.extensions.qiskit import qiskit_to_tk, tk_to_qiskit
 from pytket.utils import expectation_from_counts
 
 def mermin3():
@@ -11,18 +11,17 @@ def mermin3():
     qc = QuantumCircuit(3,3)
 
     # Foreman paper GHZ state. This yielded results as high as 3.27.
-    #qc.h(0)
-    #qc.cnot(0,1)
-    #qc.cnot(0,2)
-    #qc.s(0)
-    #qc.barrier()
-
-    # typical GHZ(+) state
     qc.h(0)
     qc.cnot(0,1)
-    qc.cnot(1,2)
+    qc.cnot(0,2)
+    qc.s(0)
     qc.barrier()
 
+    # typical GHZ(+) state
+    #qc.h(0)
+    #qc.cnot(0,1)
+    #qc.cnot(1,2)
+    #qc.barrier()
 
     return qc
 
@@ -39,6 +38,14 @@ def mermin4():
     qc.cnot(0, 3)
     qc.s(0)
     qc.barrier()
+
+    # typical GHZ(+) state
+    #qc.h(0)
+    #qc.cnot(0,1)
+    #qc.cnot(1,2)
+    #qc.cnot(2,3)
+    #qc.barrier()
+
     return qc
 
 def mermin5():
@@ -57,26 +64,30 @@ def mermin5():
     qc.s(0)
     qc.barrier()
 
+    # ghz pure
+    #qc.h(0)
+    #qc.cnot(0,1)
+    #qc.cnot(1,2)
+    #qc.cnot(2,3)
+    #qc.cnot(3,4)
+    #qc.barrier()
+
     return qc
 
 # converted the qiskit circuit to pytket circuit, so we can optimize + run now
-state=qiskit_to_tk(mermin3()).copy()
+state=qiskit_to_tk(mermin5()).copy()
 
-# the M4 and M5 will require several (10+) different measurement types. So, instead generalize this process
-#m3=["yxx", "xyx", "xxy", "yyy"]
+# These are the measurements to be performed on the 3/4/5-qubit mermin inequalities
 
-#this m3 is for the typical gh state. the above is for foreman GHZ
-m3=["yyx", "yxy", "xyy", "xxx"]
-m4=["yxxx", "xyxx", "xxyx", "xxxy", "yyxx", "yxyx", "yxxy", "xyyx", "xyxy", "xxyy", "yyyy", "xxxx", "yyyx", "yyxy", "xyyy", "xyyy"]
-m5=["yxxxx", "xyxxx", "xxyxx", "xxxyx", "xxxxy",    "yyyxx", "yyxyx", "yyxxy", "yxyyx", "yxyxy", "yxxyy", "xyyyx", "xyyxy", "xyxyy", "xxyyy", "yyyy"]
+m3=["xxy", "xyx", "yxx", "yyy"]
+coeff_3= [1.0, 1.0, 1.0, -1.0]
 
-# see mermin inequality in paper for coefficient explanations
-#coeff_3= [1.0, 1.0, 1.0, -1.0]
+m4=["xxxy", "xxyx", "xyxx", "yxxx", "xyyy", "yxyy", "yyxy", "yyyx"]
+coeff_4=[1, 1, 1, 1, -1, -1, -1, -1]
 
-# this is for typical GHZ. above is for cameron GHZ
-coeff_3= [-1.0, -1.0, -1.0, 1.0]
-coeff_4= [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1]
-coeff_5= [1,1,1,1,1, -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+m5=["xxxxy", "xxxyx", "xxyxx", "xyxxx", "yxxxx",   "xxyyy", "xyyxy", "xyyyx", "xyxyy", "yyyxx", "yyxyx", "yyxxy", "yxyyx", "yxyxy", "yxxyy", "yyyyy"]
+coeff_5=[1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1]
+
 
 def measurements(string):
     """
@@ -98,6 +109,9 @@ def measurements(string):
 
     # barrier used to isolate sections which Pytket can optimize
     qc.add_barrier(range(0,len(string)))
+    #qc.Measure(0,1)
+    #qc.Measure(1,0)
+    #qc.Measure(2,2)
     qc.measure_all()
 
     return qc
@@ -105,23 +119,23 @@ def measurements(string):
 circ_list=[]
 
 # append measurements in x/y bases
-for m in m3:
+for m in m5:
     c = state.copy()
     c.append(measurements(m))
     circ_list.append(c)
+    print(tk_to_qiskit(c))
 
-#exit(1)
+backend = IBMQBackend("ibmq_quito")
+circ_list = backend.get_compiled_circuits(circ_list, optimisation_level=0)
 
-backend = IBMQBackend("ibmq_lima")
-circ_list = backend.get_compiled_circuits(circ_list, optimisation_level=2)
-
-handle_list = backend.process_circuits(circ_list, n_shots=20000)
+handle_list = backend.process_circuits(circ_list, n_shots=16384)
 result_list = backend.get_results(handle_list)
 
 expectation = 0
-for coeff, result in zip(coeff_3, result_list):
+for coeff, result in zip(coeff_5, result_list):
     counts = result.get_counts()
     expectation += coeff * expectation_from_counts(counts)
+    print(expectation_from_counts(counts), coeff)
 
 # computed value of the mermin polynomial
 print("final expectation: ", expectation)
